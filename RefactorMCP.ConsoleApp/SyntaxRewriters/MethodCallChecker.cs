6d68e279adc5e0db3e505b74c905dfb6c25a5444
Dave Hillier <dave.hillier1@gmail.com>
2025-06-13 16:28:58 +0100

    Split syntax rewriters into individual files

---
+++ RefactorMCP.ConsoleApp/SyntaxRewriters/MethodCallChecker.cs
@@ -0,0 +1,31 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class MethodCallChecker : CSharpSyntaxRewriter
+{
+    private readonly HashSet<string> _classMethodNames;
+    public bool HasMethodCalls { get; private set; }
+
+    public MethodCallChecker(HashSet<string> classMethodNames)
+    {
+        _classMethodNames = classMethodNames;
+    }
+
+    public override SyntaxNode? VisitInvocationExpression(InvocationExpressionSyntax node)
+    {
+        if (node.Expression is IdentifierNameSyntax identifier)
+        {
+            if (_classMethodNames.Contains(identifier.Identifier.ValueText))
+            {
+                HasMethodCalls = true;
+            }
+        }
+
+        return base.VisitInvocationExpression(node);
+    }
+}
+
---
+++ RefactorMCP.ConsoleApp/SyntaxRewriters/MethodCallRewriter.cs
@@ -0,0 +1,37 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class MethodCallRewriter : CSharpSyntaxRewriter
+{
+    private readonly HashSet<string> _classMethodNames;
+    private readonly string _parameterName;
+
+    public MethodCallRewriter(HashSet<string> classMethodNames, string parameterName)
+    {
+        _classMethodNames = classMethodNames;
+        _parameterName = parameterName;
+    }
+
+    public override SyntaxNode? VisitInvocationExpression(InvocationExpressionSyntax node)
+    {
+        if (node.Expression is IdentifierNameSyntax identifier)
+        {
+            if (_classMethodNames.Contains(identifier.Identifier.ValueText))
+            {
+                var memberAccess = SyntaxFactory.MemberAccessExpression(
+                    SyntaxKind.SimpleMemberAccessExpression,
+                    SyntaxFactory.IdentifierName(_parameterName),
+                    identifier);
+
+                return node.WithExpression(memberAccess);
+            }
+        }
+
+        return base.VisitInvocationExpression(node);
+    }
+}
+
