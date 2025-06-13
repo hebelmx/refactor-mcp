bf978d8bbf4f7684d81e24e49faab0fdc4d72d91
Dave Hillier <dave.hillier1@gmail.com>
2025-06-13 13:47:37 +0100

    Update SyntaxRewriters.cs

--- RefactorMCP.ConsoleApp/Tools/SyntaxRewriters.cs
+++ RefactorMCP.ConsoleApp/Tools/SyntaxRewriters.cs
@@ -1,6 +1,7 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 using System.Collections.Generic;
 using System.Linq;
 
