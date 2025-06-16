4de3a7c7484b69d33f8205ada56605f89c68b50e
Dave Hillier <dave.hillier1@gmail.com>
2025-06-16 23:52:00 +0100

    Refactor: move BodyOmitter to separate file

--- RefactorMCP.ConsoleApp/Tools/SummaryResources.cs
+++ RefactorMCP.ConsoleApp/Tools/SummaryResources.cs
@@ -34,16 +34,4 @@ public static class SummaryResources
         return sb.ToString();
     }
 
-    private class BodyOmitter : CSharpSyntaxRewriter
-    {
-        public override SyntaxNode? VisitBlock(BlockSyntax node)
-        {
-            return SyntaxFactory.Block(SyntaxFactory.ParseStatement("// ...\n"));
-        }
-
-        public override SyntaxNode? VisitArrowExpressionClause(ArrowExpressionClauseSyntax node)
-        {
-            return SyntaxFactory.Block(SyntaxFactory.ParseStatement("// ...\n"));
-        }
-    }
 }
