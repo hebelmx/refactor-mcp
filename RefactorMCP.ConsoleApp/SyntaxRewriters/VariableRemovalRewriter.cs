2f32985a12147023ab8ab8b089ec37fcb95dc9e9
Dave Hillier <dave.hillier1@gmail.com>
2025-06-17 17:02:28 +0100

    Introduce generic declaration removal rewriter

--- RefactorMCP.ConsoleApp/SyntaxRewriters/VariableRemovalRewriter.cs
+++ RefactorMCP.ConsoleApp/SyntaxRewriters/VariableRemovalRewriter.cs
@@ -2,30 +2,26 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
-using System.Collections.Generic;
 using System.Linq;
 
-public class VariableRemovalRewriter : CSharpSyntaxRewriter
+namespace RefactorMCP.ConsoleApp.SyntaxRewriters;
+
+internal class VariableRemovalRewriter : DeclarationRemovalRewriter<LocalDeclarationStatementSyntax>
 {
-    private readonly string _variableName;
     private readonly TextSpan _span;
 
     public VariableRemovalRewriter(string variableName, TextSpan span)
+        : base(variableName)
     {
-        _variableName = variableName;
         _span = span;
     }
 
-    public override SyntaxNode? VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
-    {
-        var variable = node.Declaration.Variables.FirstOrDefault(v => v.Identifier.ValueText == _variableName && v.Span.Equals(_span));
-        if (variable == null)
-            return base.VisitLocalDeclarationStatement(node);
+    protected override bool IsTarget(LocalDeclarationStatementSyntax node)
+        => node.Declaration.Variables.Any(v => v.Identifier.ValueText == Name && v.Span.Equals(_span));
 
-        if (node.Declaration.Variables.Count == 1)
-            return null;
+    protected override SeparatedSyntaxList<VariableDeclaratorSyntax>? GetDeclarators(LocalDeclarationStatementSyntax node)
+        => node.Declaration.Variables;
 
-        var newDecl = node.Declaration.WithVariables(SyntaxFactory.SeparatedList(node.Declaration.Variables.Where(v => v != variable)));
-        return node.WithDeclaration(newDecl);
-    }
+    protected override LocalDeclarationStatementSyntax WithDeclarators(LocalDeclarationStatementSyntax node, SeparatedSyntaxList<VariableDeclaratorSyntax> declarators)
+        => node.WithDeclaration(node.Declaration.WithVariables(declarators));
 }
