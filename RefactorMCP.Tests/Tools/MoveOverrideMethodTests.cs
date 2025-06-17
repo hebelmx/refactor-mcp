541dffa5e3be691b81caa1b29b6885d5b1379998
Dave Hillier <dave.hillier1@gmail.com>
2025-06-17 15:32:25 +0100

    Remove override modifier when moving to new class

---
+++ RefactorMCP.Tests/Tools/MoveOverrideMethodTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Xunit;
+
+namespace RefactorMCP.Tests;
+
+public class MoveOverrideMethodTests
+{
+    [Fact]
+    public void MoveOverrideMethod_RemovesOverrideInNewClass()
+    {
+        var source = @"public class Base { public virtual void Foo() {} } public class Derived : Base { public override void Foo() {} }";
+        var tree = CSharpSyntaxTree.ParseText(source);
+        var root = tree.GetRoot();
+
+        var moveResult = MoveMethodsTool.MoveInstanceMethodAst(root, "Derived", "Foo", "Target", "", "");
+        var updatedRoot = MoveMethodsTool.AddMethodToTargetClass(moveResult.NewSourceRoot, "Target", moveResult.MovedMethod, moveResult.Namespace);
+        var formattedRoot = Formatter.Format(updatedRoot, new AdhocWorkspace());
+
+        var targetClass = formattedRoot.DescendantNodes().OfType<ClassDeclarationSyntax>()
+            .First(c => c.Identifier.ValueText == "Target");
+        var movedMethod = targetClass.Members.OfType<MethodDeclarationSyntax>().First();
+        Assert.DoesNotContain("override", movedMethod.Modifiers.ToFullString());
+
+        var derivedClass = formattedRoot.DescendantNodes().OfType<ClassDeclarationSyntax>()
+            .First(c => c.Identifier.ValueText == "Derived");
+        var stub = derivedClass.Members.OfType<MethodDeclarationSyntax>().First(m => m.Identifier.ValueText == "Foo");
+        Assert.Contains("override", stub.Modifiers.ToFullString());
+    }
+}
