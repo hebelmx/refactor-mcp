ed5bc1be1c18c107ed4b2c07b2ade4dd2d803b70
Dave Hillier <dave.hillier1@gmail.com>
2025-06-20 15:44:41 +0100

    fix MoveMultipleMethods duplicate method bug

---
+++ RefactorMCP.Tests/Tools/MoveMultipleMethodsDuplicateTests.cs
@@ -0,0 +1,33 @@
+using ModelContextProtocol;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RefactorMCP.Tests;
+
+public class MoveMultipleMethodsDuplicateTests : TestBase
+{
+    [Fact]
+    public async Task MoveMultipleMethods_DuplicateNames_Fails()
+    {
+        UnloadSolutionTool.ClearSolutionCache();
+        var testFile = Path.Combine(TestOutputPath, "DupMethods.cs");
+        var code = @"public class Source { public void A() { } } public class Target { }";
+        await TestUtilities.CreateTestFile(testFile, code);
+        await LoadSolutionTool.LoadSolution(SolutionPath, null, CancellationToken.None);
+        var solution = await RefactoringHelpers.GetOrLoadSolution(SolutionPath);
+        var project = solution.Projects.First();
+        RefactoringHelpers.AddDocumentToProject(project, testFile);
+
+        var result = await MoveMultipleMethodsTool.MoveMultipleMethods(
+            SolutionPath,
+            testFile,
+            "Source",
+            new[] { "A", "A" },
+            "Target");
+
+        Assert.Contains("Duplicate method names", result);
+    }
+}
