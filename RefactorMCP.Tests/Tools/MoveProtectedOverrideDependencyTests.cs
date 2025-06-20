9cb9d978896c7d8b7c908996d70fb72a1d0af45a
Dave Hillier <dave.hillier1@gmail.com>
2025-06-20 11:50:11 +0100

    Fix access level update for protected override methods

---
+++ RefactorMCP.Tests/Tools/MoveProtectedOverrideDependencyTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Xunit;
+
+namespace RefactorMCP.Tests;
+
+public class MoveProtectedOverrideDependencyTests
+{
+    [Fact]
+    public void MoveMethod_UpdatesProtectedOverrideDependency()
+    {
+        var source = @"public class Base { protected virtual void DoIt() {} } public class Derived : Base { protected override void DoIt() {} public void Another() { DoIt(); } } public class Target { }";
+        var tree = CSharpSyntaxTree.ParseText(source);
+        var root = tree.GetRoot();
+
+        var moveResult = MoveMethodsTool.MoveInstanceMethodAst(root, "Derived", "Another", "Target", "", "");
+        var updatedRoot = MoveMethodsTool.AddMethodToTargetClass(moveResult.NewSourceRoot, "Target", moveResult.MovedMethod, moveResult.Namespace);
+        var formattedRoot = Formatter.Format(updatedRoot, new AdhocWorkspace());
+
+        var derivedClass = formattedRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().First(c => c.Identifier.ValueText == "Derived");
+        var method = derivedClass.Members.OfType<MethodDeclarationSyntax>().First(m => m.Identifier.ValueText == "DoIt");
+        var mods = method.Modifiers.ToFullString();
+
+        Assert.Contains("protected", mods);
+        Assert.Contains("internal", mods);
+        Assert.Contains("override", mods);
+    }
+}
